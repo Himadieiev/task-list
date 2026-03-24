d5360e9f3f9baa85267947fc4231facb0059ad7f
Himadieiev Ruslan <gimadejev86@gmail.com>
2026-03-24 15:40:21 +0200

    Add markup, styles, assets

---
+++ src/styles/index.js
@@ -0,0 +1,10 @@
+import "./normalize.css";
+import "./fonts.css";
+import "./variables.css";
+import "./utils.css";
+import "./globals.css";
+
+import "./components/button.css";
+import "./components/field.css";
+import "./components/task.css";
+import "./components/task-item.css";
